3be696ac6e476343abff3685af35a8e74e9654c9
Souheil CHELFOUH <trollfot@gmail.com>
2012-01-19 00:37:16 +0100

    Added files that are components splitted from the main file

---
+++ src/dolmen/app/container/components.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import dolmen.menu
+from cromlech.browser import slot
+from dolmen.app.layout import master, menus
+from dolmen.app.container import listing, addtofolder
+from dolmen.app.security import permissions
+from grokcore.security import require, order
+
+
+class AddMenu(addtofolder.AddMenuViewlet):
+    order(20)
+    slot(master.AboveBody)
+    require(permissions.CanAddContent)
+
+
+@dolmen.menu.menuentry(menus.ContextualMenu)
+class Listing(listing.ListingPage):
+    order(50)
+    require(permissions.ListFolder)
